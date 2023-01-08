8aaf4ad13b1ad80cc6fde0fbd83e779bd7466511
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-08 09:05:42 +0530

    day-1

--- Components/Song.tsx
+++ Components/Song.tsx
@@ -5,18 +5,18 @@ function Song({order,track}) {
     const spotifyApi = useSpotify();
 
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500">
         <div className="flex items-center space-x-4">
             <p>{order+1}</p>
             <img className="h-10 w-10" src={track.track.album.images[0].url} alt="" />
             <div>
-                <p>{track.track.name}</p>
+                <p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
                 <p>{track.track.artists[0].name}</p>
             </div>
         </div>
         <div className="flex items-center justify-between ml-auto md:ml-0">
             <p className="hidden md:inline">{track.track.album.name}</p>
-            <p>{millisToMinutsAndSecond(track.track.duration)}</p>
+            <p>{millisToMinutsAndSecond(track.track.duration_ms)}</p>
         </div>
     </div>
   )
