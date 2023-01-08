abc01b31da4b578214a978b5a06bd151a0049dc9
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-09 01:08:02 +0530

    day-2

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -24,11 +24,6 @@ function Sidebar() {
   return (
     <div className='text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'>
       <div className='space-y-4'>
-      <button className='flex items-center space-x-2 hover:text-white'
-      onClick={()=>signOut()}>
-            <HomeIcon className='h-5 w-5'/>
-            <p>Log out</p>
-        </button>
         <button className='flex items-center space-x-2 hover:text-white'>
             <HomeIcon className='h-5 w-5'/>
             <p>Home</p>
--- Components/Song.tsx
+++ Components/Song.tsx
@@ -5,7 +5,7 @@ function Song({order,track}) {
     const spotifyApi = useSpotify();
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
         <div className="flex items-center space-x-4">
             <p>{order+1}</p>
             <img className="h-10 w-10" src={track.track.album.images[0].url} alt="" />
