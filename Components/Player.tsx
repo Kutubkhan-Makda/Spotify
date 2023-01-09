379e11e06b56c881bce29274b2a85c0837cf60bf
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-10 01:22:46 +0530

    day-3

--- Components/Player.tsx
+++ Components/Player.tsx
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
@@ -12,11 +12,31 @@ function Player() {
     const [isPlaying,setIsPlaying] = useRecoilState(isPlayingState);
     const [volume, setVolume] = useState(50)
     const songInfo = useSongInfo();
+    const fetchCurrentSong = ()=> {
+      if(!songInfo){
+        spotifyApi.getMyCurrentPlayingTrack().then((data:any)=>{setCurrentTrackId(data.body?.item?.id)
+        spotifyApi.getMyCurrentPlaybackState().then((data)=>{
+          setIsPlaying(data.body?.is_playing)
+        })})
+      }
+    }
+
+    useEffect(()=>{
+      if(spotifyApi.getAccessToken() && !currentTrackId)
+      {
+        fetchCurrentSong();
+        setVolume(50);
+      }
+    },[currentTrackId,spotifyApi,session])
 
   return (
-    <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white">
         <div>
             <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+            <div>
+              <h3>{songInfo?.name}</h3>
+              <p>{songInfo?.artists?.[0]?.name}</p>
+            </div>
         </div>
     </div>
   )
--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -22,7 +22,7 @@ function Sidebar() {
   },[session,spotifyApi])
 
   return (
-    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex'>
+    <div className='text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36'>
       <div className='space-y-4'>
         <button className='flex items-center space-x-2 hover:text-white'>
             <HomeIcon className='h-5 w-5'/>
